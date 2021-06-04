c660836f1df7f4250113c95d945eb31158efb42c
andres <andres.cortes.escobedo@080formacion.alzago.es>
2021-06-04 19:43:40 +0200

    frontend

---
+++ seleccion.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/style.css">
+    <link rel="shortcut icon" href="./img/icono.ico" type="image/x-icon">
+    <title>Bingo!
+        
+    </title>
+</head>
+<body>
+<div class="cuerpo">
+  <header class="cabecera">Header</header>
+  <article class="main">
+    <div class="bola"><div class="numero">
+        5
+        </div>
+    </div>  
+  </article>
+  <aside class="lateral lateral-1">
+  <ul class="jugadores">
+  <li> <img src="./img/default.png" height="250px">  jugador1</li>
+  <li><img src="./img/default.png" height="250px">jugador 3</li>
+  </ul>
+  </aside>
+  <aside class="lateral lateral-2">
+  <ul class="jugadores">
+  <li><img src="./img/default.png" height="250px">jugador 2</li>
+  <li><img src="./img/default.png" height="250px">jugador 4</li>
+  </ul>
+  </aside>
+  <footer class="footer">Footer</footer>
+</div>
+</body>
+</html>
