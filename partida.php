493dfb18ca16863f1585837139d12fa99c4f2ae2
rafahitgub <rafiya8f1@gmail.com>
2021-06-05 20:36:03 +0200

    reestructuracion pagina de partida

---
+++ partida.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="./css/estilosPartida.css">
+    <title>Bingo</title>
+</head>
+<body>
+    <header></header>
+    <main>
+        <section class="jugadores jugadoresTop">
+            <article class="jugadorContenedor">
+                <img class="imgJugador"/>
+                <h2 class="nombreJugadoor">Antonio</h2>
+                <div class="cartonesContenedor">
+
+                </div>
+            </article>
+            <article class="jugadorContenedor">
+                <img class="imgJugador"/>
+                <h2 class="nombreJugadoor">Antonio</h2>
+                <div class="cartonesContenedor">
+
+                </div>
+            </article>
+        </section>
+        <section class="jugadores jugadoresBottom">
+            <article class="jugadorContenedor">
+                <img class="imgJugador"/>
+                <h2 class="nombreJugadoor">Antonio</h2>
+                <div class="cartonesContenedor">
+
+                </div>
+            </article>
+            <article class="jugadorContenedor">
+                <img class="imgJugador"/>
+                <h2 class="nombreJugadoor">Antonio</h2>
+                <div class="cartonesContenedor">
+
+                </div>
+            </article>
+        </section>
+        <section id="tablero">
+
+        </section>
+        <section id="ventanaLog"></section>
+    </main>
+    <footer></footer>
+</body>
+</html>
