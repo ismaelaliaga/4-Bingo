ee49fa52fee9f3508ef6c20727016671e635c0f5
rafahitgub <rafiya8f1@gmail.com>
2021-06-07 14:37:30 +0200

    adicion de contenedor de ventana de log y contenedor tablero(la bola y boton de sacar bola). Imagenes de jugadores modeladas.

--- partida.php
+++ partida.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -10,17 +10,28 @@
 <body>
     <header></header>
     <main>
+        <section id="tablero">
+            <h2 id="bola">
+                5
+            </h2>
+            <button id="sacarBolaBoton">Sacar bola</button>
+        </section>
+        <div id="ventanaLogContenedor">
+            <section id="ventanaLog">
+                ¡Bienvenidos a la partida!
+            </section>
+        </div>
         <section class="jugadores jugadoresTop">
             <article class="jugadorContenedor">
-                <img class="imgJugador"/>
+                <img class="imgJugador" src="./img/jugadores/0.jpg"/>
                 <h2 class="nombreJugadoor">Antonio</h2>
                 <div class="cartonesContenedor">
 
                 </div>
             </article>
             <article class="jugadorContenedor">
-                <img class="imgJugador"/>
-                <h2 class="nombreJugadoor">Antonio</h2>
+                <img class="imgJugador" src="./img/jugadores/1.jpg"/>
+                <h2 class="nombreJugadoor">Felipe</h2>
                 <div class="cartonesContenedor">
 
                 </div>
@@ -28,24 +39,20 @@
         </section>
         <section class="jugadores jugadoresBottom">
             <article class="jugadorContenedor">
-                <img class="imgJugador"/>
-                <h2 class="nombreJugadoor">Antonio</h2>
+                <img class="imgJugador" src="./img/jugadores/2.jpg"/>
+                <h2 class="nombreJugadoor">Gabriel</h2>
                 <div class="cartonesContenedor">
 
                 </div>
             </article>
             <article class="jugadorContenedor">
-                <img class="imgJugador"/>
+                <img class="imgJugador" src="./img/jugadores/3.jpg"/>
                 <h2 class="nombreJugadoor">Antonio</h2>
                 <div class="cartonesContenedor">
 
                 </div>
             </article>
         </section>
-        <section id="tablero">
-
-        </section>
-        <section id="ventanaLog"></section>
     </main>
     <footer></footer>
 </body>
