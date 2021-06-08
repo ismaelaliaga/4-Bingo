52098d9a8da902c38752ee59f3c2155b3cbe0b33
rafahitgub <rafiya8f1@gmail.com>
2021-06-08 18:18:18 +0200

    me quitaránel archivo pero no las ganas de luchar. Aquí está de vuelta

--- src/Bingo/partida.php
+++ src/Bingo/partida.php
@@ -1,126 +1,72 @@
-<?php
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="../../css/estilosPartida.css">
+    <title>Bingo</title>
+</head>
+<body>
+    <header>
+        <nav id="menu">
+                <ul>
+                    <li><img id="logoMenu" src="../../img/logot.png"/></li>
+                    <form method="post" id="formMenu">
+                        <li><button class="botonMenu">Reiniciar</button></li>
+                        <li><button class="botonMenu">Finalizar</button></li>
+                    </form>
+                </ul>
+        </nav>
+    </header>
+    <main>
+        <section id="tablero">
+            <span>Tirada 10</span>
+            <h2 id="bola">
+                5
+            </h2>
+            <button id="sacarBolaBoton">Sacar bola</button>
+        </section>
+        <div id="ventanaLogContenedor">
+            <section id="ventanaLog">
+            <ul>
+                <li>¡Bienvenidos a la partida! ¡Buenas suerte!</li>
+            </ul>
+            </section>
+        </div>
+        <section class="jugadores jugadoresTop">
+            <article class="jugadorContenedor">
+                <img class="imgJugador" src="./img/jugadores/0.jpg"/>
+                <h2 class="nombreJugadoor">Antonio</h2>
+                <div class="cartonesContenedor">
 
-declare(strict_types=1);
+                </div>
+            </article>
+            <article class="jugadorContenedor">
+                <img class="imgJugador" src="../../img/jugadores/1.jpg"/>
+                <h2 class="nombreJugadoor">Felipe</h2>
+                <div class="cartonesContenedor">
 
-require_once ('Bingo.php');
-require_once ('Bombo.php');
-require_once ('Jugador.php');
-require_once ('Carton.php');
+                </div>
+            </article>
+        </section>
+        <section class="jugadores jugadoresBottom">
+            <article class="jugadorContenedor">
+                <img class="imgJugador" src="../../img/jugadores/2.jpg"/>
+                <h2 class="nombreJugadoor">Gabriel</h2>
+                <div class="cartonesContenedor">
 
-// $partida = new Bingo("Ismael", 1, "Andrés", 2, "Rafa", 1, "Alberto", 1);
-// $arrayConLosJugadores = $partida->getJugadores();
-function crearPartia($nombreJ1, $cartonesJ1, $nombreJ2, $cartonesJ2, $nombreJ3, $cartonesJ3,
-$nombreJ4, $cartonesJ4){
-    $partida = new Bingo("$nombreJ1", $cartonesJ1, "$nombreJ2", $cartonesJ2, "$nombreJ3", $cartonesJ3,
-    "$nombreJ4", $cartonesJ4);
-    return $partida;
-}
-$partidaComenzada = crearpartia("Ismael", 1, "Andrés", 2, "Rafa", 3, "Alberto", 1);
+                </div>
+            </article>
+            <article class="jugadorContenedor">
+                <img class="imgJugador" src="../../img/jugadores/3.jpg"/>
+                <h2 class="nombreJugadoor">Antonio</h2>
+                <div class="cartonesContenedor">
 
-$arrayConLosJugadores = $partidaComenzada->getJugadores();
-$jugador1 = $arrayConLosJugadores[0];
-$jugador2 = $arrayConLosJugadores[1];
-$jugador3 = $arrayConLosJugadores[2];
-$jugador4 = $arrayConLosJugadores[3];
-
-$arrayConLosCartonesDelJugador1 = $jugador1->getCartones();
-var_dump($jugador1->getCartones());
-$primerCartonDelJugador1 = $arrayConLosCartonesDelJugador1[0];
-
-$arrayConLosCartonesDelJugador2 = $jugador2->getCartones();
-$primerCartonDelJugador2 = $arrayConLosCartonesDelJugador1[0];
-$segundoCartonDelJugador2 = $arrayConLosCartonesDelJugador1[1];
-
-$arrayConLosCartonesDelJugador3 = $jugador3->getCartones();
-$primerCartonDelJugador3 = $arrayConLosCartonesDelJugador1[0];
-$segundoCartonDelJugador3 = $arrayConLosCartonesDelJugador1[1];
-$tercerCartonDelJugador3 = $arrayConLosCartonesDelJugador1[2];
-
-$arrayConLosCartonesDelJugador4 = $jugador4->getCartones();
-
-// echo "El primer jugador se llama " . $arrayConLosJugadores[0]->getNombre() . " y tiene ";
-// $arrayConLosCartonesDelJ1 = $arrayConLosJugadores[0]->getCartones();
-// if($arrayConLosCartonesDelJ1[0]){
-//     $idDelPrimerCarton = $arrayConLosCartonesDelJ1[0]->devolverID();
-//     echo "el cartón $idDelPrimerCarton";
-// }
-// // if($arrayConLosCartonesDelJ1[1]){
-// //     $idDelSegundoCarton = $arrayConLosCartonesDelJ1[1]->devolverID();
-// //     echo ", el cartón $idDelSegundoCarton";
-// // }
-// // if($arrayConLosCartonesDelJ1[2]){
-// //     $idDelTercerCarton = $arrayConLosCartonesDelJ1[2]->devolverID();
-// //     echo ", el cartón $idDelTercerCarton";
-// // }
-// echo ".</br>";
-
-// echo "El segundo jugador se llama " . $arrayConLosJugadores[1]->getNombre() . " y tiene ";
-// $arrayConLosCartonesDelJ2 = $arrayConLosJugadores[1]->getCartones();
-// if($arrayConLosCartonesDelJ2[0]){
-//     $idDelPrimerCarton = $arrayConLosCartonesDelJ2[0]->devolverID();
-//     echo "el cartón $idDelPrimerCarton";
-// }
-// // if($arrayConLosCartonesDelJ1[1]){
-// //     $idDelSegundoCarton = $arrayConLosCartonesDelJ1[1]->devolverID();
-// //     echo ", el cartón $idDelSegundoCarton";
-// // }
-// // if($arrayConLosCartonesDelJ1[2]){
-// //     $idDelTercerCarton = $arrayConLosCartonesDelJ1[2]->devolverID();
-// //     echo ", el cartón $idDelTercerCarton";
-// // }
-// echo ".</br>";
-// $arrayConLosCartonesDelJ2[1];
-
-// echo "El tercer jugador se llama " . $arrayConLosJugadores[2]->getNombre() . " y tiene ";
-// $arrayConLosCartonesDelJ3 = $arrayConLosJugadores[2]->getCartones();
-// if($arrayConLosCartonesDelJ3[0]){
-//     $idDelPrimerCarton = $arrayConLosCartonesDelJ3[0]->devolverID();
-//     echo "el cartón $idDelPrimerCarton";
-// }
-// // if($arrayConLosCartonesDelJ1[1]){
-// //     $idDelSegundoCarton = $arrayConLosCartonesDelJ1[1]->devolverID();
-// //     echo ", el cartón $idDelSegundoCarton";
-// // }
-// // if($arrayConLosCartonesDelJ1[2]){
-// //     $idDelTercerCarton = $arrayConLosCartonesDelJ1[2]->devolverID();
-// //     echo ", el cartón $idDelTercerCarton";
-// // }
-// echo ".</br>";
-
-// echo "El cuarto jugador se llama " . $arrayConLosJugadores[3]->getNombre() . " y tiene ";
-// $arrayConLosCartonesDelJ4 = $arrayConLosJugadores[3]->getCartones();
-// if($arrayConLosCartonesDelJ4[0]){
-//     $idDelPrimerCarton = $arrayConLosCartonesDelJ4[0]->devolverID();
-//     echo "el cartón $idDelPrimerCarton";
-// }
-// // if($arrayConLosCartonesDelJ1[1]){
-// //     $idDelSegundoCarton = $arrayConLosCartonesDelJ1[1]->devolverID();
-// //     echo ", el cartón $idDelSegundoCarton";
-// // }
-// // if($arrayConLosCartonesDelJ1[2]){
-// //     $idDelTercerCarton = $arrayConLosCartonesDelJ1[2]->devolverID();
-// //     echo ", el cartón $idDelTercerCarton";
-// // }
-// echo ".</br>";
-
-// $bombo = new Bombo;
-// $primeraBola = $bombo->sacarBola();
-// echo "El crupier saca la primera bola, que el $primeraBola.</br>";
-// echo $arrayConLosCartonesDelJ1[0]->buscarNumeroEnElCarton($primeraBola) . "</br>";
-// echo $arrayConLosCartonesDelJ2[0]->buscarNumeroEnElCarton($primeraBola) . "</br>";
-// echo $arrayConLosCartonesDelJ3[0]->buscarNumeroEnElCarton($primeraBola) . "</br>";
-// echo $arrayConLosCartonesDelJ4[0]->buscarNumeroEnElCarton($primeraBola) . "</br>";
-
-// $segundaBola = $bombo->sacarBola();
-// echo "El crupier saca la segunda, que el $segundaBola.</br>";
-// echo $arrayConLosCartonesDelJ1[0]->buscarNumeroEnElCarton($segundaBola) . "</br>";
-// echo $arrayConLosCartonesDelJ2[0]->buscarNumeroEnElCarton($segundaBola) . "</br>";
-// echo $arrayConLosCartonesDelJ3[0]->buscarNumeroEnElCarton($segundaBola) . "</br>";
-// echo $arrayConLosCartonesDelJ4[0]->buscarNumeroEnElCarton($segundaBola) . "</br>";
-
-// $terceraBola = $bombo->sacarBola();
-// echo "El crupier saca la primera bola, que el $terceraBola.</br>";
-// echo $arrayConLosCartonesDelJ1[0]->buscarNumeroEnElCarton($terceraBola) . "</br>";
-// echo $arrayConLosCartonesDelJ2[0]->buscarNumeroEnElCarton($terceraBola) . "</br>";
-// echo $arrayConLosCartonesDelJ3[0]->buscarNumeroEnElCarton($terceraBola) . "</br>";
-// echo $arrayConLosCartonesDelJ4[0]->buscarNumeroEnElCarton($terceraBola) . "</br>";
+                </div>
+            </article>
+        </section>
+    </main>
+    <footer></footer>
+</body>
+</html>
