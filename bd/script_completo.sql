d748ac3a55bacb87fd39c5bbdc43c652fea8fe73
andres <andres.cortes.escobedo@080formacion.alzago.es>
2021-06-09 21:37:26 +0200

    cambios en la base de datos

--- bd/script_completo.sql
+++ bd/script_completo.sql
@@ -2,7 +2,7 @@ DROP DATABASE IF EXISTS `bingo`;
 CREATE DATABASE `bingo`;
 USE `bingo`;
 CREATE TABLE `cartones`(`id_carton` TINYINT PRIMARY KEY AUTO_INCREMENT, `numeros` VARCHAR(150) NOT NULL, `estado_default` VARCHAR(150) NOT NULL);
-CREATE TABLE `jugadores`(`id_jugador` TINYINT PRIMARY KEY AUTO_INCREMENT, `nombre_jugador` VARCHAR(50) NOT NULL);
+CREATE TABLE `jugadores`(`id_jugador` TINYINT PRIMARY KEY AUTO_INCREMENT, `nombre_jugador` VARCHAR(50) NOT NULL , `imagen_jugador` LONGTEXT NOT NULL);
 CREATE TABLE `partida`(`id_jugador` TINYINT ,`id_carton` TINYINT , `estado` VARCHAR(150) NOT NULL, FOREIGN KEY (`id_carton`) REFERENCES `cartones`(`id_carton`), FOREIGN KEY (`id_jugador`) REFERENCES `jugadores`(`id_jugador`));
 CREATE TABLE `log`(`log` VARCHAR (150));
 INSERT INTO `cartones`(`id_carton`, `numeros`, `estado_default`) VALUES
